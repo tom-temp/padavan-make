08b1ae4228bc70a0d8749c76e48c4017fe138164
test-ning <aaa@aaa.com>
2021-09-07 17:15:04 +0800

    1

--- padavan/NEWIFI3/NEWIFI3.sh
+++ padavan/NEWIFI3/NEWIFI3.sh
@@ -18,8 +18,8 @@ echo "CONFIG_FIRMWARE_INCLUDE_HTOP=n" >>.config
 echo "CONFIG_FIRMWARE_INCLUDE_SHADOWSOCKS=y" >>.config # SS plus+
 echo "CONFIG_FIRMWARE_INCLUDE_SSSERVER=n" >>.config    # SS server
 echo "CONFIG_FIRMWARE_INCLUDE_SSOBFS=y" >>.config      # simple-obfs混淆插件,SS 开了才可以打开
-echo "CONFIG_FIRMWARE_INCLUDE_V2RAY=y" >>.config  # 集成v2ray执行文件（3.8M左右)，如果不集成，会从网上下载下来执行，不影响正常使用
-echo "CONFIG_FIRMWARE_INCLUDE_TROJAN=y" >>.config # 集成trojan执行文件(1.1M左右)，如果不集成，会从网上下载下来执行，不影响正常使用
+echo "CONFIG_FIRMWARE_INCLUDE_V2RAY=n" >>.config  # 集成v2ray执行文件（3.8M左右)，如果不集成，会从网上下载下来执行，不影响正常使用
+echo "CONFIG_FIRMWARE_INCLUDE_TROJAN=n" >>.config # 集成trojan执行文件(1.1M左右)，如果不集成，会从网上下载下来执行，不影响正常使用
 
 # 文件
 echo "CONFIG_FIRMWARE_INCLUDE_CADDY=y" >>.config    # 在线文件管理服务
@@ -27,14 +27,14 @@ echo "CONFIG_FIRMWARE_INCLUDE_CADDYBIN=n" >>.config # 集成 caddu执行文件�
 
 # 广告
 echo "CONFIG_FIRMWARE_INCLUDE_ADBYBY=n" >>.config      # adbyby plus+
-echo "CONFIG_FIRMWARE_INCLUDE_KOOLPROXY=n" >>.config   # KP 广告过滤
-echo "CONFIG_FIRMWARE_INCLUDE_ADGUARDHOME=y" >>.config # ADGUARD 广告拦截
+echo "CONFIG_FIRMWARE_INCLUDE_KOOLPROXY=y" >>.config   # KP 广告过滤
+echo "CONFIG_FIRMWARE_INCLUDE_ADGUARDHOME=n" >>.config # ADGUARD 广告拦截
 
 # 代理
 echo "CONFIG_FIRMWARE_INCLUDE_KUMASOCKS=n" >>.config # KUMA
 
 # 穿透
-echo "CONFIG_FIRMWARE_INCLUDE_FRPC=y" >>.config    # 内网穿透FRPC
+echo "CONFIG_FIRMWARE_INCLUDE_FRPC=n" >>.config    # 内网穿透FRPC
 echo "CONFIG_FIRMWARE_INCLUDE_FRPS=y" >>.config    # 内网穿透FRPS
 echo "CONFIG_FIRMWARE_INCLUDE_ALIDDNS=y" >>.config # 阿里DDNS
 echo "CONFIG_FIRMWARE_INCLUDE_ZEROTIER=y" >>.config   # zerotier ~1.3M
